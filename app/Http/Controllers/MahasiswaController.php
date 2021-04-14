43c7e4f0032895287aeb8964ebac4859f3d9297b
= <=>
2021-04-14 10:43:50 +0700

    Modifikasi MahasiswaController untuk detail

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -93,8 +93,8 @@ class MahasiswaController extends Controller
     public function show($Nim)
     {
         //menampilkan detail data dengan menemukan/berdasarkan Nim Mahasiswa
-        $Mahasiswa = Mahasiswa::find($Nim);
-        return view('mahasiswas.detail', compact('Mahasiswa'));
+        $Mahasiswa = Mahasiswa::with('kelas')->where('Nim' , $Nim)->first();
+        return view('mahasiswas.detail', ['Mahasiswa' => $Mahasiswa]);
     }
 
     /**
